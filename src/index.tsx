57fa0f74be6b2761757b89252d69a6bcc747e6cf
LCoupier <coupierlucien@gmail.com>
2020-10-20 13:26:50 +0200

    add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -9,6 +9,19 @@ import ThemeProvider from "@material-ui/styles/ThemeProvider";
 import { store } from "./redux/config";
 import { history } from "./redux/Router/router";
 import { theme } from "./style/theme";
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+	dsn: "https://c93a8f5866bb44a7a8f7af2e50cb2647@o464340.ingest.sentry.io/5472438",
+	integrations: [
+	  new Integrations.BrowserTracing(),
+	],
+  
+	// We recommend adjusting this value in production, or using tracesSampler
+	// for finer control
+	tracesSampleRate: 1.0,
+  });
 
 ReactDOM.render(
 	<Provider store={store}>
